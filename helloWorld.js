23f5156c9611d5243d33ab6c76453c8b3ad801d6
rubatocantabile <rubacocantabile@gmail.com>
2020-04-21 00:12:46 +0900

    오늘은 여기까지

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,35 @@
-console.log('Hello World');
+console.log("다시 처음부터~")
+console.warn(`마마마지막처럼 ${'마마마지막처럼'} 마마마지막인것처럼~~`)
+
+//템플릿 형식 출력하기
+
+console.log('name:%s', 'Chorong')
+console.log('age:%d', 00)
+
+//한번에 여러 정보
+//%s 문자 , %d 숫자
+console.log('name:%s age:%d', 'Chorong', 00)
+
+
+// `백틱` 포매팅
+
+const greeting1 = 'hello'
+const greeting2 = 'Namaste'
+const greeting3 = 'Annyung'
+const name1 = 'Ram'
+const name2 = 'Sol'
+const name3 = 'Do'
+
+const statement1 = `${greeting1}! my name is ${name1}`
+const statement2 = `${greeting2}! my name is ${name2}`
+const statement3 = `${greeting3}! my name is ${name3}`
+
+
+console.log(`statememt1 : ${greeting1}! my name is ${name1}`)
+console.log(`statememt2 : ${greeting2}! my name is ${name2}`)
+console.log(`statememt3 : ${greeting3}! my name is ${name3}`)
+
+console.log("statememt1 :", statement1)
+console.log("statememt2 :", statement2)
+console.log("statememt3 :", statement3)
+
